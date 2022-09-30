460bea285dec3667e80e86d3414f53fe68814d27
nave <navesasrussi@gmail.com>
2022-09-30 06:04:59 +0300

    sentence + bottom bar

--- app/src/main/java/com/example/KC/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/KC/ui/home/HomeFragment.java
@@ -14,22 +14,33 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.KC.R;
 
-public class HomeFragment extends Fragment {
+import java.util.Random;
+
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
+    private TextView textView;
+    private String[] beautiful_sentences = new String[]{"'12312311'", "'1233123'", " 'כגעגכע'  נ.ב"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+        textView.setOnClickListener(this);
         return root;
+    }
+
+    public void onClick(View arg0) {
+        Random rand = new Random();
+        textView.setText(beautiful_sentences[rand.nextInt(3)]);
     }
 }
